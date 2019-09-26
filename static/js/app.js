21e70fb0d4bc755511f1c48b5a9d4f83853f62d5
Snehal Godse <sgodse3@gmail.com>
2019-09-26 12:27:54 -0700

    Added scatter plot and filtered crime types

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,66 @@
-function init() {
-    
-    d3.json(`/pie`).then(function(response) {
+d3.json(`/pie`).then(function(response) {
         
-        console.log("inside pie chart building...");
-        crime_types = [];
-        crime_type_values = [];
-        for (const [key, value] of Object.entries(response)) {
-            crime_types.push(key);
-            crime_type_values.push(value);
+    console.log("inside pie chart building...");
+    crime_types = [];
+    crime_type_values = [];
+    for (const [key, value] of Object.entries(response)) {
+        crime_types.push(key);
+        crime_type_values.push(value);
+    }
+    var trace = {
+        type: "pie",
+        labels: crime_types.slice(0,15),
+        values: crime_type_values.slice(0, 15)
+    };
+    
+    var data = [trace];
+    var layout = {title: "Chicago Crime Types (2010-2019)"};
+
+    Plotly.newPlot("pie", data, layout);
+});
+
+d3.json('/line').then(function(response) {
+
+    console.log("inside line chart building...");
+    console.log(response);
+
+    var data = [];
+    
+    for (const [key, value] of Object.entries(response)) {
+
+        var crime_type = key;
+        var crime_data = value;
+        var crime_years = [];
+        var crime_count = [];
+
+        for (const [key, value] of Object.entries(crime_data)) {
+            crime_years.push(key);
+            crime_count.push(value);
+            
         }
         var trace = {
-            type: "pie",
-            labels: crime_types.slice(0,15),
-            values: crime_type_values.slice(0, 15)
-        };
+            x: crime_years,
+            y: crime_count,
+            mode: 'lines+markers',
+            type: 'scatter',
+            name: crime_type
+          };
+        
+        data.push(trace);
+        
+    }
+    var layout = {
+        xaxis: {
+          range: [ 2010, 2019 ],
+          title: "Year"
+        },
+        yaxis: {
+          title: "No. of occurences"
+        },
+        title:'Crimes Over Years'
+      };
+
+    Plotly.newPlot('line', data, layout);
     
-        var data = [trace];
-        var layout = {title: "Top 15 Chicago Crime Types (2015-2019)"};
+});
 
-        Plotly.newPlot("pie", data, layout);
-    });
-}
-
-init();
